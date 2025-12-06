a2c0f6a6a6a0874c85e953d8b12164cb70e2e1a6
robotsdontsleep <devkhomchenko@gmail.com>
2025-12-06 01:15:09 +0100

    initialization project

---
+++ src/lib/api/events.js
@@ -0,0 +1,11 @@
+// export async function getEvents() {
+//   const response = await fetch("/api/events", {
+//     cache: "no-store", // або next: { revalidate: 60 }
+//   });
+
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch events");
+//   }
+
+//   return response.json();
+}
